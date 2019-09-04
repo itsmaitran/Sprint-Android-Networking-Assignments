0223ba4e7f29151d5274e39440f2b18eb91591a9
itsmaisn <itsmaisn@gmail.com>
2019-09-04 13:27:06 -0700

    Step 4 - Create Employee Model.

---
+++ M03/Assignment/app/src/main/java/com/lambdaschool/httpoperations/model/Employee.kt
@@ -0,0 +1,9 @@
+package com.lambdaschool.httpoperations.model
+
+// Create a data class under a package named model and call it Employee.
+data class Employee (
+    val name: String,
+    val id: Int,
+    val age: Int,
+    val title: String
+)
